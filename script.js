700eeebe7fd0f2b913cff5018eb9dea16a2d35ea
Vivek161019 <s.viveksiva99@gmail.com>
2025-03-07 11:37:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function searchShipment() {
+    let searchValue = document.getElementById("searchBox").value;
+
+    fetch("data.json")
+        .then(response => response.json())
+        .then(data => {
+            let shipment = data.find(item => item.shipmentNumber === searchValue);
+            if (shipment) {
+                document.getElementById("result").innerText = 
+                    `Shipment ${shipment.shipmentNumber} is in Bin: ${shipment.binLocation}`;
+            } else {
+                document.getElementById("result").innerText = "Shipment not found.";
+            }
+        })
+        .catch(error => console.error("Error loading data:", error));
+}
 
